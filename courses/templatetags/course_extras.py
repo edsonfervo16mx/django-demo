316381817ceaad4d0a948306564ff5eb962dfc0f
edsonfervo16mx <edson55_@hotmail.com>
2022-01-22 17:51:53 -0500

    markdown for html content

--- courses/templatetags/course_extras.py
+++ courses/templatetags/course_extras.py
@@ -2,6 +2,9 @@ from django import template
 
 from courses.models import Course
 
+from django.utils.safestring import mark_safe
+import markdown
+
 register = template.Library()
 
 
@@ -16,3 +19,10 @@ def time_estimate(word_count):
     # Determina el numero de minutos que le toma terminar el curso
     minutes = round(word_count/2)
     return minutes
+
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+    # Convertir texto en formato html
+    html = markdown.markdown(markdown_text)
+    return mark_safe(html)
